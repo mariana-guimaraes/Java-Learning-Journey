a024f85fa6fe0a91314358a4c09bae155e3a33f7
Mariana Guimarães <mariana.guimr@gmail.com>
2026-03-21 22:31:24 +0000

    Adding ifelse, for, while, switchcase exercises

--- javaLearningJourney/src/main/java/loops/forloop/exercises/Exercise1.java
+++ javaLearningJourney/src/main/java/loops/forloop/exercises/Exercise1.java
@@ -1,9 +1,29 @@
 package main.java.loops.forloop.exercises;
 
-public class Exercise1 {
-   public Exercise1() {
-   }
+/*
+Times table for kids:
+A simple program to facilitate learning. A student types a positive number and the program shows the times table for that number.
+*/
 
-   public static void main(String[] args) {
-   }
+import java.util.Scanner;
+public class Exercise1 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Type a number:");
+		int number = scanner.nextInt();
+		
+		if(number>=0) {
+		System.out.printf("Times table for %d is:%n" , number);
+		for(int i=0; i<=number; i++) {
+			int result = i*number;
+			System.out.printf("%d * %d = %d%n", i, number, result);
+		}
+		}
+		else {
+			System.out.println("Please type a positive number.");
+		}
+		
+		scanner.close();
+	}
 }
--- javaLearningJourney/src/main/java/loops/whileloop/Exercise1.java
+++ javaLearningJourney/src/main/java/loops/whileloop/Exercise1.java
@@ -1,9 +1,47 @@
 package main.java.loops.whileloop;
 
-public class Exercise1 {
-   public Exercise1() {
-   }
+import java.util.Scanner;
 
-   public static void main(String[] args) {
-   }
+/*
+Statistics:
+This is for a gas station to understand what customers are buying. The customer types the number related to what was bought and the information is stored. 
+At the end of the day, the employee types the password (2026) and sees how many times each product was chosen.
+ */
+
+public class Exercise1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Choose one option: 1=Alcohol, 2=Gasoline, 3=Diesel.");
+		int number = sc.nextInt();
+		int sumAlcohol = 0;
+		int sumGasoline = 0;
+		int sumDiesel = 0;
+		
+		while (number!=2026) { //password is 2026
+			if(number==1) {
+				sumAlcohol ++;
+				number = sc.nextInt();
+				}
+			else if(number==2) {
+				sumGasoline ++;
+				number = sc.nextInt();
+				}
+			else if(number==3) {
+				sumDiesel ++;
+				number = sc.nextInt();
+				}
+			else { 
+				System.out.println("Choose one option: 1=Alcohol, 2=Gasoline, 3=Diesel.");
+				number = sc.nextInt();
+			}
+		}
+		
+		System.out.println("Results of the day:");
+		System.out.println("Alcohol: " + sumAlcohol);
+		System.out.println("Gasolina: " + sumGasoline);
+		System.out.println("Diesel: " + sumDiesel);
+		
+		sc.close();
+	}
 }
